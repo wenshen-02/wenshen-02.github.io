1b160c22d89fef4157373a9d9ac3013b8c699578
WenShen <vincentang20@hotmail.com>
2020-12-26 15:57:37 +0800

    1557

--- week04/script.js
+++ week04/script.js
@@ -17,3 +17,17 @@ function checkPasswordLength(){
 
 let elPassword = document.getElementById("kataLaluan");
 elPassword.onblur = checkPasswordLength;
+
+function checkPasswordLength2(txt) {
+    if (txt.length < 5) {
+        alert("Password must be more than 5 characters")
+    } else {
+        alert("Password accepted!")
+    }
+}
+
+let elPassword2 = document.getElementById("kataLaluan2");
+elPassword2.addEventListener("blur",function(){
+    let a = document.getElementById("kataLaluan2");
+    checkPasswordLength2(a.value);
+});
