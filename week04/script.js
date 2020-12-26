ce9871652846c56f7f282f332ff548f2184e9046
WenShen <vincentang20@hotmail.com>
2020-12-26 16:10:52 +0800

    1710

--- week04/script.js
+++ week04/script.js
@@ -27,7 +27,7 @@ function checkPasswordLength2(txt) {
 }
 
 let elPassword2 = document.getElementById("kataLaluan2");
-elPassword2.addEventListener("blur",function() {
+elPassword2.addEventListener("blur",function () {
     let a = document.getElementById("kataLaluan2");
     checkPasswordLength2(a.value);
 });
