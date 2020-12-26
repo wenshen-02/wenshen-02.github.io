43ccbe4a90adf820ee5086a79b3b7fddf661d9b2
WenShen <vincentang20@hotmail.com>
2020-12-26 15:42:25 +0800

    1542

--- week04/script.js
+++ week04/script.js
@@ -6,7 +6,7 @@ function transformUpperCase(){
 let elUsername = document.getElementById("namaPengguna");
 elUsername.addEventListener("keyup",transformUpperCase);
 
-function checkPassword(){
+function checkPasswordLength(){
     let password = document.getElementById("kataLaluan").value
     if(password.length < 5) {
         alert("Password must be more than 5 characters");
