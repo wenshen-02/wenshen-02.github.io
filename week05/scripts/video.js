d1cc96c4a474603d8f5448c8200bbe27f2daebc5
WenShen <vincentang20@hotmail.com>
2020-12-27 09:03:26 +0800

    0903

--- week05/scripts/video.js
+++ week05/scripts/video.js
@@ -18,4 +18,14 @@ window.onload = function() {
     freezeBtn.addEventListener("mouseup",function() {
         video.play();
     });
+
+    let seekBar = document.getElementById("seekBar");
+    seekBar.addEventListener("change", function() {
+        let time = video.duration * (seekBar.value / 100);
+        video.currentTime = time;
+    });
+    seekBar.addEventListener("timeupdate", function() {
+        let value = (100 / video.dursation) * video.currentTime;
+        seekBar.value = value
+    });
 };
