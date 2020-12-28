ae5931b9c844fc5625cd6c685cf5a34efadb3353
WenShen <vincentang20@hotmail.com>
2020-12-28 16:42:47 +0800

    1642

--- week06/scripts/ajax.js
+++ week06/scripts/ajax.js
@@ -11,6 +11,8 @@ function getRandomUser() {
             let elPhone = document.getElementById("phone");
             let elAddress = document.getElementById("address");
             let elEmail = document.getElementById("email");
+            
+            elFirstName.innerHTML = data.result[0].name.first;
 
             elJsonResult.innerHTML = this.response;
         }
