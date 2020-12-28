ae2c5d7c1939ca6df47a581c41d33e9e1a74ee3d
WenShen <vincentang20@hotmail.com>
2020-12-28 16:25:21 +0800

    1625

---
+++ week06/scripts/ajax.js
@@ -0,0 +1,25 @@
+function getRandomUser() {
+    let xhttp = new XMLHttpRequest(); //create requeset obj
+
+    xhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200){
+            let data = JSON.parse(this.response);
+            let elFirstName = document.getElementById("firstName");
+            let elLastName = document.getElementById("lastName");
+            let elJsonResult = document.getElementById("jasonResult");
+            let elUserImage = document.getElementById("userImage");
+            let elPhone = document.getElementById("phone");
+            let elAddress = document.getElementById("address");
+            let elEmail = document.getElementById("email");
+
+            elJsonResult.innerHTML = this.response;
+        }
+    }
+
+    xhttp.open("GET", "https://randomuser.me/api/", true);
+    xhttp.send();
+}
+let elGetRandomUser = document.getElementById("getRandomUser");
+elGetRandomUser.addEventListener("click", function() {
+    getRandomUser();
+});
