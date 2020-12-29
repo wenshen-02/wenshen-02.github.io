14de9af59f87ad3403ec0e3ad9fa9c18be3a4b31
WenShen <vincentang20@hotmail.com>
2020-12-29 11:04:19 +0800

    1104

--- week07/scripts/index.js
+++ week07/scripts/index.js
@@ -8,6 +8,8 @@ let app = new Vue({
 let app2 = new Vue({
     el: "#app2",
     data: {
-        message: "You loaded this page on " + new Date().toLocaleString()
+        message: "You loaded this page on " + new Date().toLocaleString(),
+        linkURL: "https://bbc.in/2LAnJs8",
+        linkText: "Google is down!"
     }
-})
+});
