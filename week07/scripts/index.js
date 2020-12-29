fe3ca2de9ef705ea1e0ae1230ca478dd418d01de
WenShen <vincentang20@hotmail.com>
2020-12-29 10:53:43 +0800

    1053

--- week07/scripts/index.js
+++ week07/scripts/index.js
@@ -4,3 +4,10 @@ let app = new Vue({
         message: "Hello Vue!"
     }
 });
+
+let app2 = new Vue({
+    el: "app2",
+    data: {
+        message: "You loaded this page on " + new Date().toLocaleString()
+    }
+})
