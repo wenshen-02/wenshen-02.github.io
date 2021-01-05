488a90806e4d6a9d84da043a38fdf7b870eee653
WenShen <vincentang20@hotmail.com>
2021-01-05 12:52:46 +0800

    1252

--- tableapp/scripts/book.js
+++ tableapp/scripts/book.js
@@ -17,8 +17,7 @@ function BookNow(guestName, guestEmail, guestPax, guestRemarks) {
     })
         .then((response) => response.json())
         .then(json => {
-            alert(json.booking.name + " added!" +
-                "The crew will contact ypu shortly to confirm your booking!");
+            alert(json.booking.name + " added!" + "  The crew will contact you to confirm your booking shortly!");
         });
 }
 
